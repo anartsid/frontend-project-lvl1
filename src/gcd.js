70a601ba8c4119f235c5f378be39fe2163c1060f
Anastasiia Sidakova <anart.sidakova@gmail.com>
2022-07-18 00:39:53 +0300

    checked by eslint

--- src/gcd.js
+++ src/gcd.js
@@ -5,27 +5,27 @@ export const gametranscription = () => 'Find the greatest common divisor of give
 
 export const question = () => {
   const n1 = Math.floor(Math.random() * 100) + 1;
-  const n2 = Math.floor(Math.random() * 100) + 1;  
+  const n2 = Math.floor(Math.random() * 100) + 1;
   const answer = readlineSync.question(`Question: ${n1}  ${n2} \n`);
   console.log(`Your answer: ${answer}`);
   return { n1, n2, answer };
 };
 
-export const correctanswer = ({ n1, n2} ) => {
+export const correctanswer = ({ n1, n2 }) => {
 
-   const min = ( n1 < n2 ) ? n1 : n2;
-   const max = ( n1 > n2 ) ? n1 : n2;
-   let cAns = '';
-   
-   for (let i = min; cAns = i; i = i - 1) {
-      if (min % i === 0) {
-         if (max % i === 0) {
-            cAns = i;
-            break;
-         }
+  const min = (n1 < n2) ? n1 : n2;
+  const max = (n1 > n2) ? n1 : n2;
+  let cAns = '';
+
+  for (let i = min; cAns = i; i -= 1) {
+    if (min % i === 0) {
+      if (max % i === 0) {
+        cAns = i;
+        break;
       }
-   }
-   return cAns;
+    }
+  }
+  return cAns;
 };
 
 export const brainGcd = () => { brainlogic(gametranscription, question, correctanswer); };
