38105d2536191327cd0026a9e892aad979f04ac5
Anastasiia Sidakova <anart.sidakova@gmail.com>
2022-08-19 11:31:28 +0300

    Returned the file cli.js and changed brain-games.js

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const firstquestion = () => {
+   const userName = readlineSync.question('May I have your name? ');
+  return `Hello, ${userName}!`;
+};
+
+export default firstquestion;
